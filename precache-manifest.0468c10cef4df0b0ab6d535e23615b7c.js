5d4108058bf5b408837a4f4340d27e5e3e8795c7
vinc3nt55 <vinc3nt55@gmail.com>
2019-03-07 12:25:58 +0800

    Updates

---
+++ precache-manifest.0468c10cef4df0b0ab6d535e23615b7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a12c00fbd95e41fa2f5",
+    "url": "/React-Info-Form/static/js/runtime~main.1a12c00f.js"
+  },
+  {
+    "revision": "6c19505a8eed394c0dfe",
+    "url": "/React-Info-Form/static/js/main.6c19505a.chunk.js"
+  },
+  {
+    "revision": "b754da70c332df298c39",
+    "url": "/React-Info-Form/static/js/1.b754da70.chunk.js"
+  },
+  {
+    "revision": "6c19505a8eed394c0dfe",
+    "url": "/React-Info-Form/static/css/main.62a8effb.chunk.css"
+  },
+  {
+    "revision": "3d501b6e2db8ae216d0e7fe177863068",
+    "url": "/React-Info-Form/index.html"
+  }
+];
